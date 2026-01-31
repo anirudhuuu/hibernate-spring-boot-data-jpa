111beb782fe3f925a83528cd9dd7c494f4c0789f
anirudhsai2 <anirudhsai2@gmail.com>
2026-01-31 17:17:26 +0530

    hibernate orm, jpa, entities and tables in spring boot jpa

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO product (price, quantity, sku, title)
+VALUES
+    (12,4, 'parle734', 'Parle Biscuit'),
+    (14.4, 1, 'pepsi123', 'Pepsi'),
+    (34.4, 10, 'coke124', 'Coca Cola'),
+    (16.4, 3, 'pepsi156', 'Mazza');
