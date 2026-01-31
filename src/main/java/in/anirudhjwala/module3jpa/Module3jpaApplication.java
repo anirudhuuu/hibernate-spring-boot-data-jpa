9947f31e0ddbbc3fbbe5883bdf1f7f4b8f21cc78
anirudhsai2 <anirudhsai2@gmail.com>
2026-01-31 12:36:09 +0530

    chore: init code starter

---
+++ src/main/java/in/anirudhjwala/module3jpa/Module3jpaApplication.java
@@ -0,0 +1,13 @@
+package in.anirudhjwala.module3jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Module3jpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Module3jpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/anirudhjwala/module3jpa/Module3jpaApplicationTests.java
@@ -0,0 +1,13 @@
+package in.anirudhjwala.module3jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Module3jpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
